5718d976513e6b69cebba1a5b4019eb1b5407158
ALUMNO-D103 <ALUMNO-D103@UCSP.AULAB>
2026-05-22 18:09:48 -0500

    prim

---
+++ prim/prim.cpp
@@ -0,0 +1,57 @@
+#include <ioStream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct arista
+{
+	int peso, destino;
+};
+
+struct menor_a_mayor
+{
+	bool operator()(const arista &a, const arista &b)
+	{
+		return a.peso > b.peso;
+	}
+};
+
+void prim(vector<vector<arista>>g, int nro_nodos)
+{
+	priority_queue<arista, vector<arista>, menor_a_mayor> pqmin;
+	vector<bool> ya_recorrido(nro_nodos, 0);
+	ya_recorrido[0] = 1;
+	for (const auto &p : g[0])
+	{
+		pqmin.push(p);
+	}
+	while(!pqmin.empty())
+	{
+		arista barata = pqmin.top();
+		pqmin.pop();
+	}
+}
+
+int main()
+{
+	int nro_nodos = 5;
+	vector <vector<arista>> grafo(nro_nodos);
+	//creando el grafo;
+	for(int i = 0; i < nro_nodos; i++)
+	{
+		int nro_destinos = 1 + rand() % 3;
+		for(int o = 0; o < nro_destinos; o++)
+		{
+			int destino = rand() % nro_nodos;
+			if(destino != i)
+			{
+				int peso = 4 + rand() % (10 - 4 + 1);
+				arista ida = {peso, destino};
+				grafo[i].push_back(ida);
+				arista retorno = {peso, i};
+				grafo[destino].push_back(retorno);
+			}
+		}
+	}
+}
